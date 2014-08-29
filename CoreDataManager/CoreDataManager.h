ff10196966c9a4c5a0fa3b1a5e715352f73a2b0d
Noah Portes Chaikin <noah.porteschaikin@carrotcreative.com>
2014-08-29 15:58:21 -0400

    Clean up CDM.

--- CoreDataManager/CoreDataManager.h
+++ CoreDataManager/CoreDataManager.h
@@ -4,10 +4,51 @@
 @interface CoreDataManager : NSObject
 
 + (NSManagedObjectContext *) managedObjectContext;
-+ (NSError *)saveContext;
+
++ (NSManagedObjectContext *) newManagedObjectContext;
+
++ (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
+                              withContext:(NSManagedObjectContext *)context;
+
 + (NSFetchRequest *)fetchRequestFromTable:(NSString *)table;
-+ (NSFetchRequest *)fetchRequestFromTable:(NSString *)table matchedByPredicate:(NSPredicate *)predicate;
-+ (NSFetchRequest *)fetchRequestFromTable: (NSString *)table matchedByPredicate:(NSPredicate *)predicate andSortDescriptors:(NSArray *)sortDescriptors;
-+ (id)insertNewObjectToTable:(NSString *)table withValues:(NSDictionary *)values;
+
++ (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
+                       matchedByPredicate:(NSPredicate *)predicate
+                              withContext:(NSManagedObjectContext *)context;
+
++ (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
+                       matchedByPredicate:(NSPredicate *)predicate;
+
++ (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
+                      withSortDescriptors:(NSArray *)sortDescriptors
+                              withContext:(NSManagedObjectContext *)context;
+
++ (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
+                      withSortDescriptors:(NSArray *)sortDescriptors;
+
++ (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
+                       matchedByPredicate:(NSPredicate *)predicate
+                       withSortDescriptors:(NSArray *)sortDescriptors
+                              withContext:(NSManagedObjectContext *)context;
+
++ (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
+                       matchedByPredicate:(NSPredicate *)predicate
+                       withSortDescriptors:(NSArray *)sortDescriptors;
+
++ (NSManagedObject *)newObjectInTable:(NSString *)table
+                               withContext:(NSManagedObjectContext *)context;
+
++ (NSManagedObject *)newObjectInTable:(NSString *)table;
+
++ (NSManagedObject *)newObjectInTable:(NSString *)table
+                           withValues:(NSDictionary *)values
+                          withContext:(NSManagedObjectContext *)context;
+
++ (NSManagedObject *)newObjectInTable:(NSString *)table
+                           withValues:(NSDictionary *)values;
+
++ (NSError *) saveContext:(NSManagedObjectContext *)managedObjectContext;
+
++ (NSError *) saveContext;
 
 @end
--- CoreDataManager/CoreDataManager.m
+++ CoreDataManager/CoreDataManager.m
@@ -1,54 +1,154 @@
-#import <Foundation/Foundation.h>
-#import <CoreData/CoreData.h>
+#import "CoreDataManager.h"
 
-@interface CoreDataManager : NSObject
+NSString * const databaseName = @"databaseName";
 
-+ (NSManagedObjectContext *) managedObjectContext;
+static NSManagedObjectModel *managedObjectModel;
+static NSPersistentStoreCoordinator *persistentStoreCoordinator;
+static NSManagedObjectContext *managedObjectContext;
 
-+ (NSManagedObjectContext *) newManagedObjectContext;
+@implementation CoreDataManager
+
++ (NSManagedObjectContext *)managedObjectContext {
+    if (!managedObjectContext) {
+        managedObjectContext = [self newManagedObjectContext];
+    }
+    return managedObjectContext;
+}
+
++ (NSManagedObjectContext *)newManagedObjectContext {
+    NSManagedObjectContext *context = [[NSManagedObjectContext alloc] init];
+    context.persistentStoreCoordinator = [self persistentStoreCoordinator];
+    return context;
+}
 
 + (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
-                              withContext:(NSManagedObjectContext *)context;
+                              withContext:(NSManagedObjectContext *)context {
+    return [self fetchRequestFromTable:table
+                    matchedByPredicate:nil
+                   withSortDescriptors:nil
+                           withContext:context];
+}
 
-+ (NSFetchRequest *)fetchRequestFromTable:(NSString *)table;
++ (NSFetchRequest *)fetchRequestFromTable:(NSString *)table {
+    return [self fetchRequestFromTable:table
+                    matchedByPredicate:nil
+                   withSortDescriptors:nil
+                           withContext:[self managedObjectContext]];
+}
 
 + (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
                        matchedByPredicate:(NSPredicate *)predicate
-                              withContext:(NSManagedObjectContext *)context;
+                              withContext:(NSManagedObjectContext *)context {
+    return [self fetchRequestFromTable:table
+                    matchedByPredicate:predicate
+                   withSortDescriptors:nil
+                           withContext:context];
+}
 
 + (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
-                       matchedByPredicate:(NSPredicate *)predicate;
+                       matchedByPredicate:(NSPredicate *)predicate {
+    return [self fetchRequestFromTable:table
+                    matchedByPredicate:predicate
+                   withSortDescriptors:nil
+                           withContext:[self managedObjectContext]];
+}
 
 + (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
                       withSortDescriptors:(NSArray *)sortDescriptors
-                              withContext:(NSManagedObjectContext *)context;
+                              withContext:(NSManagedObjectContext *)context {
+    return [self fetchRequestFromTable:table
+                    matchedByPredicate:nil
+                   withSortDescriptors:sortDescriptors
+                           withContext:context];
+}
 
 + (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
-                      withSortDescriptors:(NSArray *)sortDescriptors;
+                       withSortDescriptors:(NSArray *)sortDescriptors {
+    return [self fetchRequestFromTable:table
+                    matchedByPredicate:nil
+                    withSortDescriptors:sortDescriptors
+                           withContext:[self managedObjectContext]];
+}
 
 + (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
                        matchedByPredicate:(NSPredicate *)predicate
-                       andSortDescriptors:(NSArray *)sortDescriptors
-                              withContext:(NSManagedObjectContext *)context;
+                       withSortDescriptors:(NSArray *)sortDescriptors
+                              withContext:(NSManagedObjectContext *)context {
+    NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:table];
+    request.predicate = predicate;
+    request.sortDescriptors = sortDescriptors;
+    return request;
+}
 
 + (NSFetchRequest *)fetchRequestFromTable:(NSString *)table
                        matchedByPredicate:(NSPredicate *)predicate
-                       andSortDescriptors:(NSArray *)sortDescriptors;
+                       withSortDescriptors:(NSArray *)sortDescriptors {
+    return [self fetchRequestFromTable:table
+                    matchedByPredicate:predicate
+                    withSortDescriptors:sortDescriptors
+                           withContext:[self managedObjectContext]];
+}
 
 + (NSManagedObject *)newObjectInTable:(NSString *)table
-                               withContext:(NSManagedObjectContext *)context;
+                               withContext:(NSManagedObjectContext *)context {
+    NSManagedObject *entity = [NSEntityDescription insertNewObjectForEntityForName:table
+                                  inManagedObjectContext:context];
+    return entity;
+}
 
-+ (NSManagedObject *)newObjectInTable:(NSString *)table;
++ (NSManagedObject *)newObjectInTable:(NSString *)table {
+    return [self newObjectInTable:table
+                      withContext:[self managedObjectContext]];
+}
 
 + (NSManagedObject *)newObjectInTable:(NSString *)table
                            withValues:(NSDictionary *)values
-                          withContext:(NSManagedObjectContext *)context;
+                          withContext:(NSManagedObjectContext *)context {
+    NSManagedObject *entity = [self newObjectInTable:table
+                              withContext:context];
+    for (NSString *key in values.allKeys) {
+        [entity setValue:[values valueForKey:key]
+                  forKey:key];
+    }
+    return entity;
+}
 
 + (NSManagedObject *)newObjectInTable:(NSString *)table
-                           withValues:(NSDictionary *)values;
+                           withValues:(NSDictionary *)values {
+    return [self newObjectInTable:table
+                       withValues:values
+                      withContext:[self managedObjectContext]];
+}
 
-+ (NSError *) saveContext:(NSManagedObjectContext *)managedObjectContext;
++ (NSError *) saveContext:(NSManagedObjectContext *)managedObjectContext {
+    NSError *error = nil;
+    [managedObjectContext save:&error];
+    return error;
+}
 
-+ (NSError *) saveContext;
++ (NSError *) saveContext {
+    return [self saveContext:[self managedObjectContext]];
+}
+
+#pragma mark - Private methods
+
++ (NSManagedObjectModel *) managedObjectModel {
+    if (!managedObjectModel) {
+        NSURL *modelURL = [[NSBundle mainBundle] URLForResource:databaseName
+                                                  withExtension:@"momd"];
+        managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    }
+    return managedObjectModel;
+}
+
++ (NSPersistentStoreCoordinator *) persistentStoreCoordinator {
+    if (!persistentStoreCoordinator) {
+        persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+        NSURL *applicationDocumentsDirectory = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+        NSURL *storeURL = [applicationDocumentsDirectory URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.sqlite", databaseName]];
+        [persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:nil];
+    }
+    return persistentStoreCoordinator;
+}
 
 @end
